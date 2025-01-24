2f87323ba146ca9821c950e1786a2138dc9b4605
Jorge Herrera <jorgeherrera@David-macBook.local>
2025-01-24 11:07:14 -0500

    Angular 18 directive is used in the template and styles are corrected

--- src/app/features/sidebar/components/sidebar-checklist/sidebar-checklist.component.ts
+++ src/app/features/sidebar/components/sidebar-checklist/sidebar-checklist.component.ts
@@ -7,13 +7,20 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   standalone: true,
 })
 export class SidebarChecklistComponent {
-  @Input() checklist: { id: string; dueDate?: string; checked: boolean, label: string }[] = [];
-  @Output() checklistUpdate = new EventEmitter<{ id: string; checked: boolean, label: string }>();
+  @Input() checklist: { id: string; dueDate?: string; checked: boolean; label: string }[] = [];
+  @Output() checklistUpdate = new EventEmitter<{ id: string; checked: boolean; label: string }>();
 
   toggleCheckbox(itemId: string, event: Event): void {
     const inputElement = event.target as HTMLInputElement;
     if (inputElement) {
       this.checklistUpdate.emit({ id: itemId, checked: inputElement.checked, label: '' });
     }
+  }
+
+  isOverdue(dueDate?: string): boolean {
+    if (!dueDate) return false;
+    const today = new Date();
+    const itemDate = new Date(dueDate);
+    return itemDate < today;
   }
 }
