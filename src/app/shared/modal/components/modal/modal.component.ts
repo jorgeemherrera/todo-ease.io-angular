0935df475902d26ff7636ca19929c2a967c41a5b
jorgeHerrera <150965762+jorgehittechhub@users.noreply.github.com>
2025-01-24 10:09:33 -0500

    done popup

--- src/app/shared/modal/components/modal/modal.component.ts
+++ src/app/shared/modal/components/modal/modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-modal',
+  selector: 'app-modal', 
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
   standalone: true,
